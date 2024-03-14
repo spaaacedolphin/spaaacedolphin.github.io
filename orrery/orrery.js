98416dd2ae6e37657eb2ce93fa076534b1249894
SpaaaceDolphin <109275829+dolphinlsm04@users.noreply.github.com>
2024-03-15 02:47:47 +0900

    Add files via upload

--- orrery/orrery.js
+++ orrery/orrery.js
@@ -4,7 +4,8 @@ var device=1;
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
   canvas.width=7000;
   canvas.height=7000;
-  rescale_btn.style.fontSize="32px";
+  rescale_btn.style.fontSize="64px";
+  rescale_btn.style.padding="20px 40px";
   device=2;
 }
 const ctx = canvas.getContext("2d");
@@ -12,6 +13,19 @@ ctx.translate(canvas.width/2, canvas.height/2);
 ctx.scale(1,-1);
 const model_scale = 1.4*10**(-6);
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const turbo_map = [[48,18,59],[50,21,67],[51,24,74],[52,27,81],[53,30,88],[54,33,95],[55,36,102],[56,39,109],[57,42,115],[58,45,121],[59,47,128],[60,50,134],[61,53,139],[62,56,145],[63,59,151],[63,62,156],[64,64,162],[65,67,167],[65,70,172],[66,73,177],[66,75,181],[67,78,186],[68,81,191],[68,84,195],[68,86,199],[69,89,203],[69,92,207],[69,94,211],[70,97,214],[70,100,218],[70,102,221],[70,105,224],[70,107,227],[71,110,230],[71,113,233],[71,115,235],[71,118,238],[71,120,240],[71,123,242],[70,125,244],[70,128,246],[70,130,248],[70,133,250],[70,135,251],[69,138,252],[69,140,253],[68,143,254],[67,145,254],[66,148,255],[65,150,255],[64,153,255],[62,155,254],[61,158,254],[59,160,253],[58,163,252],[56,165,251],[55,168,250],[53,171,248],[51,173,247],[49,175,245],[47,178,244],[46,180,242],[44,183,240],[42,185,238],[40,188,235],[39,190,233],[37,192,231],[35,195,228],[34,197,226],[32,199,223],[31,201,221],[30,203,218],[28,205,216],[27,208,213],[26,210,210],[26,212,208],[25,213,205],[24,215,202],[24,217,200],[24,219,197],[24,221,194],[24,222,192],[24,224,189],[25,226,187],[25,227,185],[26,228,182],[28,230,180],[29,231,178],[31,233,175],[32,234,172],[34,235,170],[37,236,167],[39,238,164],[42,239,161],[44,240,158],[47,241,155],[50,242,152],[53,243,148],[56,244,145],[60,245,142],[63,246,138],[67,247,135],[70,248,132],[74,248,128],[78,249,125],[82,250,122],[85,250,118],[89,251,115],[93,252,111],[97,252,108],[101,253,105],[105,253,102],[109,254,98],[113,254,95],[117,254,92],[121,254,89],[125,255,86],[128,255,83],[132,255,81],[136,255,78],[139,255,75],[143,255,73],[146,255,71],[150,254,68],[153,254,66],[156,254,64],[159,253,63],[161,253,61],[164,252,60],[167,252,58],[169,251,57],[172,251,56],[175,250,55],[177,249,54],[180,248,54],[183,247,53],[185,246,53],[188,245,52],[190,244,52],[193,243,52],[195,241,52],[198,240,52],[200,239,52],[203,237,52],[205,236,52],[208,234,52],[210,233,53],[212,231,53],[215,229,53],[217,228,54],[219,226,54],[221,224,55],[223,223,55],[225,221,55],[227,219,56],[229,217,56],[231,215,57],[233,213,57],[235,211,57],[236,209,58],[238,207,58],[239,205,58],[241,203,58],[242,201,58],[244,199,58],[245,197,58],[246,195,58],[247,193,58],[248,190,57],[249,188,57],[250,186,57],[251,184,56],[251,182,55],[252,179,54],[252,177,54],[253,174,53],[253,172,52],[254,169,51],[254,167,50],[254,164,49],[254,161,48],[254,158,47],[254,155,45],[254,153,44],[254,150,43],[254,147,42],[254,144,41],[253,141,39],[253,138,38],[252,135,37],[252,132,35],[251,129,34],[251,126,33],[250,123,31],[249,120,30],[249,117,29],[248,114,28],[247,111,26],[246,108,25],[245,105,24],[244,102,23],[243,99,21],[242,96,20],[241,93,19],[240,91,18],[239,88,17],[237,85,16],[236,83,15],[235,80,14],[234,78,13],[232,75,12],[231,73,12],[229,71,11],[228,69,10],[226,67,10],[225,65,9],[223,63,8],[221,61,8],[220,59,7],[218,57,7],[216,55,6],[214,53,6],[212,51,5],[210,49,5],[208,47,5],[206,45,4],[204,43,4],[202,42,4],[200,40,3],[197,38,3],[195,37,3],[193,35,2],[190,33,2],[188,32,2],[185,30,2],[183,29,2],[180,27,1],[178,26,1],[175,24,1],[172,23,1],[169,22,1],[167,20,1],[164,19,1],[161,18,1],[158,16,1],[155,15,1],[152,14,1],[149,13,1],[146,11,1],[142,10,1],[139,9,2],[136,8,2],[133,7,2],[129,6,2],[126,5,2],[122,4,3]];
+function getColor(x){
+  x = Math.round(x*12)+128;
+  if(x>255){
+    x=255;
+    console.log("over");
+  }
+  else if (x<0) {
+    x=0;
+    console.log("under");
+  }
+  return turbo_map[x];
+}
 
 var btn_cnt = 0;
 var orbit_width = 1;
@@ -73,9 +87,10 @@ function drawMercury(){
   //console.log("/"+mercury_data.substring(start_idx+123, end_idx+123)+"/");
   //console.log("/"+mercury_data.substring(start_idx+246, end_idx+246)+"/");
 
-  ctx.beginPath();
-  ctx.moveTo(mercury_pos[0], mercury_pos[1]);
+  var prev_pos = [mercury_pos[0],mercury_pos[1],mercury_pos[2]];
   for(i=1;i<=mercury_period;i++){
+    ctx.beginPath();
+    ctx.moveTo(prev_pos[0],prev_pos[1]);
     var orbit_pos = mercury_data.substring(start_idx+123*i, end_idx+123*i);
     orbit_pos=orbit_pos.split(",");
     for(j=0;j<3;j++){
@@ -83,10 +98,14 @@ function drawMercury(){
       orbit_pos[j]= Number(temp[0])*(10**Number(temp[1]));
       orbit_pos[j]= orbit_pos[j]*model_scale;
     }
+    var color = getColor(orbit_pos[2]);
+    ctx.strokeStyle="rgb("+color[0]+","+color[1]+","+color[2]+")";
+    //console.log(ctx.strokeStyle);
     ctx.lineTo(orbit_pos[0],orbit_pos[1]);
+    ctx.stroke();
+    prev_pos = orbit_pos;
   }
-  ctx.strokeStyle = "gray";
-  ctx.stroke();
+  //ctx.strokeStyle = "gray";
 
   ctx.beginPath();
   ctx.arc(mercury_pos[0], mercury_pos[1], mercury_radius, 0, 2*Math.PI);
@@ -114,9 +133,10 @@ function drawVenus(){
   }
   console.log(venus_pos);
 
-  ctx.beginPath();
-  ctx.moveTo(venus_pos[0], venus_pos[1]);
+  var prev_pos = [venus_pos[0],venus_pos[1],venus_pos[2]];
   for(i=1;i<=venus_period;i++){
+    ctx.beginPath();
+    ctx.moveTo(prev_pos[0], prev_pos[1]);
     var orbit_pos = venus_data.substring(start_idx+123*i, end_idx+123*i);
     orbit_pos=orbit_pos.split(",");
     for(j=0;j<3;j++){
@@ -124,10 +144,13 @@ function drawVenus(){
       orbit_pos[j]= Number(temp[0])*(10**Number(temp[1]));
       orbit_pos[j]= orbit_pos[j]*model_scale;
     }
+    var color = getColor(orbit_pos[2]);
+    ctx.strokeStyle="rgb("+color[0]+","+color[1]+","+color[2]+")";
+    //console.log(ctx.strokeStyle);
     ctx.lineTo(orbit_pos[0],orbit_pos[1]);
+    ctx.stroke();
+    prev_pos = orbit_pos;
   }
-  ctx.strokeStyle = "gray";
-  ctx.stroke();
 
   ctx.beginPath();
   ctx.arc(venus_pos[0], venus_pos[1], venus_radius, 0, 2*Math.PI);
@@ -154,9 +177,10 @@ function drawEarth(){
   }
   console.log(earth_pos);
 
-  ctx.beginPath();
-  ctx.moveTo(earth_pos[0], earth_pos[1]);
+  var prev_pos = [earth_pos[0],earth_pos[1],earth_pos[2]];
   for(i=1;i<=earth_period;i++){
+    ctx.beginPath();
+    ctx.moveTo(prev_pos[0], prev_pos[1]);
     var orbit_pos = earth_data.substring(start_idx+123*i, end_idx+123*i);
     orbit_pos=orbit_pos.split(",");
     for(j=0;j<3;j++){
@@ -164,10 +188,13 @@ function drawEarth(){
       orbit_pos[j]= Number(temp[0])*(10**Number(temp[1]));
       orbit_pos[j]= orbit_pos[j]*model_scale;
     }
+    var color = getColor(orbit_pos[2]);
+    ctx.strokeStyle="rgb("+color[0]+","+color[1]+","+color[2]+")";
+    //console.log(ctx.strokeStyle);
     ctx.lineTo(orbit_pos[0],orbit_pos[1]);
+    ctx.stroke();
+    prev_pos = orbit_pos;
   }
-  ctx.strokeStyle = "gray";
-  ctx.stroke();
 
   ctx.beginPath();
   ctx.arc(earth_pos[0], earth_pos[1], earth_radius, 0, 2*Math.PI);
@@ -194,9 +221,10 @@ function drawMars(){
   }
   console.log(mars_pos);
 
-  ctx.beginPath();
-  ctx.moveTo(mars_pos[0], mars_pos[1]);
+  var prev_pos = [mars_pos[0],mars_pos[1],mars_pos[2]];
   for(i=1;i<=mars_period;i++){
+    ctx.beginPath();
+    ctx.moveTo(prev_pos[0], prev_pos[1]);
     var orbit_pos = mars_data.substring(start_idx+123*i, end_idx+123*i);
     orbit_pos=orbit_pos.split(",");
     for(j=0;j<3;j++){
@@ -204,10 +232,13 @@ function drawMars(){
       orbit_pos[j]= Number(temp[0])*(10**Number(temp[1]));
       orbit_pos[j]= orbit_pos[j]*model_scale;
     }
+    var color = getColor(orbit_pos[2]);
+    ctx.strokeStyle="rgb("+color[0]+","+color[1]+","+color[2]+")";
+    //console.log(ctx.strokeStyle);
     ctx.lineTo(orbit_pos[0],orbit_pos[1]);
+    ctx.stroke();
+    prev_pos = orbit_pos;
   }
-  ctx.strokeStyle = "gray";
-  ctx.stroke();
 
   ctx.beginPath();
   ctx.arc(mars_pos[0], mars_pos[1], mars_radius, 0, 2*Math.PI);
