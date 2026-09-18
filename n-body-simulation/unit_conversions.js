620482000c60a200bfebd307800198b8122187e6
SpaceDolphin <dolphin.lsm04@gmail.com>
2026-09-18 23:31:24 +0900

    notes updated

--- n-body-simulation/unit_conversions.js
+++ n-body-simulation/unit_conversions.js
@@ -1,5 +1,5 @@
 function toggle() {
-  var x = document.getElementById("unit_conversions_div");
+  let x = document.getElementById("unit_conversions_div");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
@@ -7,26 +7,22 @@ function toggle() {
   }
 }
 
-var distance_input = document.getElementById("distance_conversion_input");
-var distance_units = document.getElementById("distance_units");
-var distance_output = document.getElementById("distance_conversion_output");
-var year = document.getElementById("year");
-var month = document.getElementById("month");
-var day = document.getElementById("day");
-var hour = document.getElementById("hour");
-var minute = document.getElementById("minute");
-var second = document.getElementById("second");
-var time_output = document.getElementById("time_conversion_output");
-var mass_input = document.getElementById("mass_conversion_input");
-var mass_units = document.getElementById("mass_units");
-var mass_output = document.getElementById("mass_conversion_output");
-
-function isEmptyString(str) {
-  return str.length === 0;
-}
+let distance_input = document.getElementById("distance_conversion_input");
+let distance_units = document.getElementById("distance_units");
+let distance_output = document.getElementById("distance_conversion_output");
+let year = document.getElementById("year");
+let month = document.getElementById("month");
+let day = document.getElementById("day");
+let hour = document.getElementById("hour");
+let minute = document.getElementById("minute");
+let second = document.getElementById("second");
+let time_output = document.getElementById("time_conversion_output");
+let mass_input = document.getElementById("mass_conversion_input");
+let mass_units = document.getElementById("mass_units");
+let mass_output = document.getElementById("mass_conversion_output");
 
 function EmptyString_to_0(str){
-  if(isEmptyString(str)){
+  if(!str){
     return 0;
   }
   else{
@@ -35,8 +31,8 @@ function EmptyString_to_0(str){
 }
 
 function update_distance_input() {
-  var distance_input_value = EmptyString_to_0(distance_input.value);
-  var output;
+  let distance_input_value = EmptyString_to_0(distance_input.value);
+  let output;
   switch (distance_units.value) {
     case "km":
       output = distance_input_value * 1000;
@@ -48,10 +44,10 @@ function update_distance_input() {
       output = distance_input_value * 9460730472580800;
       break;
     case "pc":
-      output = distance_input_value * ((648000/Math.PI) * 149597870700);
+      output = distance_input_value * ((648000.0/Math.PI) * 149597870700.0);
       break;
   }
-  distance_output.value = output;
+  distance_output.value = output.toExponential();
 }
 
 function update_distance_units() {
@@ -60,65 +56,40 @@ function update_distance_units() {
 
 //-----------------------------//
 
-function update_year() {
-  update_time_output()
-}
-
-function update_month() {
-  update_time_output()
-}
-
-function update_day() {
-  update_time_output()
-}
-
-function update_hour() {
-  update_time_output()
-}
-
-function update_minute() {
-  update_time_output()
-}
-
-function update_second() {
-  update_time_output()
-}
-
 function day_to_sec(d){
   return d * 86400;
 }
 
 function update_time_output(){
-  var year_value = EmptyString_to_0(year.value);
-  var month_value = EmptyString_to_0(month.value);
-  var day_value = EmptyString_to_0(day.value);
-  var hour_value = EmptyString_to_0(hour.value);
-  var minute_value = EmptyString_to_0(minute.value);
-  var second_value = EmptyString_to_0(second.value);
-  var output = day_to_sec(year_value * 365.2425) + day_to_sec(month_value * 30.436875) + day_to_sec(day_value) + (3600 * hour_value) + (60 * minute_value) + (1*second_value);
+  let year_value = EmptyString_to_0(year.value);
+  let day_value = EmptyString_to_0(day.value);
+  let hour_value = EmptyString_to_0(hour.value);
+  let minute_value = EmptyString_to_0(minute.value);
+  let second_value = EmptyString_to_0(second.value);
+  let output = day_to_sec(year_value * 365.25) + day_to_sec(day_value) + (3600 * hour_value) + (60 * minute_value) + (1*second_value);
   time_output.value = output;
 }
 
 //-----------------------------//
 
 function update_mass_input() {
-  var mass_input_value = EmptyString_to_0(mass_input.value);
-  var output;
+  let mass_input_value = EmptyString_to_0(mass_input.value);
+  let output;
   switch (mass_units.value) {
     case "M☉":
-      output = mass_input_value * 1.98847*(10**30);
+      output = mass_input_value * 1.98847e+30;
       break;
     case "MJ":
-      output = mass_input_value * 1.89813*(10**27);
+      output = mass_input_value * 1.89813e+27;
       break;
     case "M🜨":
-      output = mass_input_value * 5.9722*(10**24);
+      output = mass_input_value * 5.9722e+24;
       break;
     case "M☾":
-      output = mass_input_value * 7.342*(10**22);
+      output = mass_input_value * 7.342e+22;
       break;
   }
-  mass_output.value = output;
+  mass_output.value = output.toExponential();
 }
 
 function update_mass_units() {
